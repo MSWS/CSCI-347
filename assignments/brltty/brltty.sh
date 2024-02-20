8d8d81521c37792af02796f6ca7c0ca879e2c8fa
MSWS <imodmaker@gmail.com>
2024-02-20 15:32:55 -0800

    Working

--- assignments/brltty/brltty.sh
+++ assignments/brltty/brltty.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -n "Number of braile drivers: " && grep -c "^#braille-driver" brltty.conf.rtf
+echo -n "Number of braile drivers: " && grep -c "^#braille-driver" $1
 
 echo "List of braile devices:"
-grep "#braille-device" brltty.conf.rtf | grep -Eo "\s#.+" | sed -E "s/.+# /\t/g" | sed "s/\.\\\//g"
+grep "#braille-device" $1| grep -Eo "\s#.+" | sed -E "s/.+# /\t/g" | sed "s/\.\\\//g" 
+echo "Number of text tables: $(grep -c "^#text-table" $1)"
+echo "Number of contraction tables: $(grep -c "^#contraction-table" $1)"
